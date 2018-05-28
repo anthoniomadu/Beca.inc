cee845d64c0fbf1862b4e6adc80360b3f759ea0d
PinkCode15 <juliehiva@gmail.com>
2018-05-28 23:48:56 +0100

    Stacey added this folder

---
+++ clothe line/js/film.js
@@ -0,0 +1,9 @@
+$('.carousel').carousel()
+$('.carousel').carousel({
+  interval: 500,
+  pause:"hover",
+  keyboard:true
+})
+$('.carousel').carousel('cycle')
+$('.carousel').carousel('prev')
+$('.carousel').carousel('next')
